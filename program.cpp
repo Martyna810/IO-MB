9818842ea8b56e0376876b902015a8e7bf8e6458
Martyna810 <martyna21519@gmail.com>
2024-12-03 15:13:17 +0100

    Dodaj funkcje silni do program.cpp

--- program.cpp
+++ program.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
+long long silnia (int n) {
+	if (n <= 1) return 1;
+	return n * silnia (n-1);
+}
+
 int main(){
 int x;
 cin >> x;
-cout << x << endl;
+cout << silnia(x) << endl;
 return 0;
 }
